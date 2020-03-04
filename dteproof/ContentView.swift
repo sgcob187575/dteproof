61eca9a0a7b7776ca893c245d9cda34a4996d507
YuHao <sgcob187575@gmail.com>
2020-03-04 15:21:53 +0800

    Initial Commit

---
+++ dteproof/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  dteproof
+//
+//  Created by 陳昱豪 on 2020/3/4.
+//  Copyright © 2020 Chen_Yu_Hao. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
