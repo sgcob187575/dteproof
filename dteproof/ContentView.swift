9de40f6dff4da03b90fd0514c3e07b716aa9d520
YuHao <sgcob187575@gmail.com>
2020-04-01 15:28:18 +0800

    oktalog

--- dteproof/ContentView.swift
+++ dteproof/ContentView.swift
@@ -8,6 +8,41 @@
 
 import SwiftUI
 import FacebookLogin
+func authn(account:String,password:String) {
+    let api_token = "001_jfExIBUFpjSq6m5SCXKDGN4XfPg7Vvj_PXipxd"
+    let domain = "dev-261926.okta.com"
+    let query="https://\(domain)/api/v1/authn"
+    if let urlStr = query.addingPercentEncoding(withAllowedCharacters:
+        .urlQueryAllowed){
+        
+        if let url = URL(string: urlStr) {
+            let authorization = "SSWS \(api_token)"
+            let content_type="application/json"
+            
+            
+            
+            var request = URLRequest(url: url)
+            let log=Log(username: account, password: password)
+            if let data = try? JSONEncoder().encode(log){
+                print(String(data: data, encoding: .utf8))
+                request.httpMethod = "POST"
+                request.setValue(content_type, forHTTPHeaderField: "Content-Type")
+                request.setValue(authorization, forHTTPHeaderField: "Authorization")
+                request.httpBody=data
+                URLSession.shared.dataTask(with: request) { (retdata, response , error) in
+                    if let retdata = retdata,let content=String(data: retdata, encoding: .utf8){
+                        print(content)
+                    }
+                    else{
+                        print(error)
+                    }
+                }.resume()
+                
+            }
+        }
+    }
+}
+
 struct ContentView: View {
     let manager = LoginManager()
     @State var ID = ""
@@ -17,6 +52,11 @@ struct ContentView: View {
     var body: some View {
         
         VStack {
+            Button(action: {
+                authn(account: "jack1@test.com", password: "Abcd1234")
+            }, label: {
+                Text("okta登入")
+            })
             Button(action: {
                 
                 let manager = LoginManager()
@@ -29,33 +69,33 @@ struct ContentView: View {
                     
                 }
                 if let accessToken = AccessToken.current {
-                   Profile.loadCurrentProfile { (profile, error) in
-                       if let profile = profile {
-                        self.imageURL="\(profile.imageURL(forMode: .square, size: CGSize(width: 300, height: 300))!)"
-                        
+                    Profile.loadCurrentProfile { (profile, error) in
+                        if let profile = profile {
+                            self.imageURL="\(profile.imageURL(forMode: .square, size: CGSize(width: 300, height: 300))!)"
+                            
+                        }
                     }
-                   }
                 }
                 let request = GraphRequest(graphPath: "me", parameters: ["fields": "id, email, name"])
                 request.start { (response, result, error) in
-                   if let result = result as? [String:String] {
-                    self.ID=result["id"]!
-                    self.email=result["email"]!
-                    self.name=result["name"]!
-                   }
+                    if let result = result as? [String:String] {
+                        self.ID=result["id"]!
+                        self.email=result["email"]!
+                        self.name=result["name"]!
+                    }
                 }
                 
             }
-            
+                
                 , label: {Text("登入")})
             Text(ID)
             Text(email)
             Text(name)
             Text(imageURL)
-
-        }
             
-}
+        }
+        
+    }
 }
 
 struct ContentView_Previews: PreviewProvider {
